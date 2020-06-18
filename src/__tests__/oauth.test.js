1926bde67d25062ea7f2ca0a58465729db2997ec
FayeCarter <fayecarter333@gmail.com>
2020-06-18 23:23:46 +0100

    remove coverage folder

--- src/__tests__/oauth.test.js
+++ src/__tests__/oauth.test.js
@@ -2,7 +2,6 @@ const request = require("supertest");
 const server = require("../../index");
 
 describe("Test the api login route", () => {
-  
   test("GET should should redirect", async () => {
     const res = await request(server).get("/api/login");
     expect(res.status).toBe(302);
