a1af2c6be95ad7823ac2bea92013ac4bd4b45f77
FayeCarter <fayecarter333@gmail.com>
2020-06-19 14:32:12 +0100

    rename client directory

---
+++ frontend/src/__tests__/App.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../App.js";
+
+describe("Application Testing", () => {
+  let wrapper;
+
+  beforeEach( () => {
+    wrapper = shallow(<App />);
+  });
+
+  test("renders header with `Spotify App` text", () => {
+    expect(wrapper.find('h1').text()).toContain("Spotify App")
+  });
+
+  test("renders `Enter with Spotify` button", () => {
+    expect(wrapper.find('#enter-button').text()).toBe("Enter with Spotify")
+  });
+
+  test("`enter-button` redirects to server route", () => {
+    jest.spyOn(window.location, "assign").mockImplementation()
+    wrapper.find("#enter-button").simulate("click");
+    expect(window.location.assign).toHaveBeenCalledWith("http://localhost:5000/api/login");
+  });
+})
