2ebfa55742e8723b2d23dbce5c21237d20bfae3d
FayeCarter <fayecarter333@gmail.com>
2020-06-19 10:31:49 +0100

    update login route test

--- src/__tests__/oauth.test.js
+++ src/__tests__/oauth.test.js
@@ -2,9 +2,11 @@ const request = require("supertest");
 const server = require("../../index");
 
 describe("Test the api login route", () => {
-  test("GET should should redirect", async () => {
-    const res = await request(server).get("/api/login");
-    expect(res.status).toBe(302);
+  test("GET should should redirect", (done) => {
+    request(server)
+          .get('/api/login')
+          .expect('Location', /accounts.spotify.com/)
+          .expect(302, done);
   });
   
 });
