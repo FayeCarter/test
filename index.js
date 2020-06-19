2bb47dea585a65cc200cfb3fcd6e8b531618db3e
FayeCarter <fayecarter333@gmail.com>
2020-06-19 10:51:36 +0100

    create /api/login/callback route

--- index.js
+++ index.js
@@ -9,19 +9,41 @@ require("dotenv").config();
 
 const redirect_uri = process.env.REDIRECT_URI
 
-
 app.get("/api", cors(), async (req, res) => {
   res.status(200).send("Hello World!");
 })
 
 app.get('/api/login', cors(), function(req, res) {
-  res.redirect('https://accounts.spotify.com/authorize?' +
+  res.redirect("https://accounts.spotify.com/authorize?" +
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      scope: "user-read-private user-read-email",
       redirect_uri
     }))
   });
+
+app.get("/api/login/callback", function(req, res) {
+  let code = req.query.code || null
+  let authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: "authorization_code"
+    },
+    headers: {
+      "Authorization": "Basic " + (Buffer.from(
+        process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
+      ).toString('base64'))
+    },
+    json: true
+  }
+  request.post(authOptions, function(error, response, body) {
+    var access_token = body.access_token
+    let uri = "http://localhost:3000/user"
+    res.redirect(uri + "?access_token=" + access_token)
+  })
+})
 
 module.exports = app;
--- src/__tests__/oauth.test.js
+++ src/__tests__/oauth.test.js
@@ -1,12 +1,18 @@
 const request = require("supertest");
 const server = require("../../index");
 
-describe("Test the api login route", () => {
-  test("GET should should redirect", (done) => {
+describe("Spotify OAuth redirection", () => {
+  test("GET /api/login should should redirect", (done) => {
     request(server)
-          .get('/api/login')
-          .expect('Location', /accounts.spotify.com/)
+          .get("/api/login")
+          .expect("Location", /accounts.spotify.com/)
           .expect(302, done);
   });
   
+  test("GET /api/login/callback should should redirect", (done) => {
+    request(server)
+          .get("/api/login/callback")
+          .expect("Location", /localhost:3000\/user/ )
+          .expect(302, done);
+  });
 });
