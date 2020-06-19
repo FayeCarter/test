071cc36306a37fe07c680d91ed0212b11255d426
FayeCarter <fayecarter333@gmail.com>
2020-06-19 15:04:13 +0100

    remove app.js comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 // import { Route, Link } from "react-router-dom";
 
 function App() {
+
+  componentDidMount() {
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token;
+    if (!accessToken)
+      return;
+    fetch('https://api.spotify.com/v1/me', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => this.setState({
+      user: {
+        name: data.display_name
+      }
+    }))
+  }
+
   return (
     <div className="App">
       <h1>Spotify App</h1>
@@ -9,7 +25,6 @@ function App() {
           id="enter-button"
           onClick={() => {
             const url = "http://localhost:5000/api/login"
-            // window.location.href = url
             window.location.assign(url)
           }}
         >
