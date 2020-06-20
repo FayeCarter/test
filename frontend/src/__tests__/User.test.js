bfa4c1aec1a62a5ec369be81c1b9e6eedea12b97
FayeCarter <fayecarter333@gmail.com>
2020-06-20 16:09:03 +0100

    test spotify call

--- frontend/src/__tests__/User.test.js
+++ frontend/src/__tests__/User.test.js
@@ -2,15 +2,26 @@ import React from "react";
 import { shallow } from "enzyme";
 import User from "../components/User.js";
 
+
+const fetch = jest.fn(() => Promise.resolve());
+
 describe("Application Testing", () => {
   let wrapper;
 
   beforeEach( () => {
-    wrapper = shallow(<User />);
+    wrapper = shallow(<User authToken={process.env.TOKEN} />);
   });
 
-  test("User calls spotify api to get details", () => {
+  test("Username element exists",  () => {
+    expect(wrapper.find(".username").exists()).toEqual(true);
+  });
 
+  test('user data from spotify', () => {
+    // const getSpy = jest.spyOn(fetch, 'get');
+    // const user = shallow(
+    //   <User/>
+    // );
+    // expect(getSpy).toBeCalled();
   });
 
 })
