e3bea157682ddb51a852787b84689257be7984d8
FayeCarter <fayecarter333@gmail.com>
2020-06-20 14:39:18 +0100

    test join route

---
+++ frontend/src/__tests__/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import Dashboard from '../components/Dashboard';
+import Join from '../components/Join';
+import App from '../App';
+
+describe("Application routing Testing", () => {
+  let wrapper;
+
+  beforeEach( () => {
+    wrapper = mount(<App />);
+  });
+
+  test("rRenders join route", () => {
+    expect(wrapper.html()).toContain('Join')
+  });
+
+})
--- frontend/src/__tests__/Join.test.js
+++ frontend/src/__tests__/Join.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import Join from "../components/Join.js";
 
-describe("Application Testing", () => {
+describe("Join page Testing", () => {
   let wrapper;
 
   beforeEach( () => {
